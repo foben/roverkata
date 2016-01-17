b5840fa2cafa51a85c830af57453676fe1843953
Felix Obenauer <f.obenauer@gmail.com>
2016-01-17 14:45:38 +0100

    Add tests for default, custom and illegal setup

--- tests.py
+++ tests.py
@@ -19,5 +19,32 @@ class TestApiSetup(unittest.TestCase):
         rover = Rover()
         self.assertIsInstance(rover, Rover)
 
+    def test_default_setup(self):
+        rover = Rover()
+        self.assertEqual(rover.get_position(), (0,0))
+        self.assertEqual(rover.get_orientation(), 'N')
+        self.assertEqual(rover.get_width(), 5)
+        self.assertEqual(rover.get_height(), 5)
+
+    def test_custom_setup(self):
+        rover = Rover(5, 5, 'S', 10, 10)
+        self.assertEqual(rover.get_position(), (5,5))
+        self.assertEqual(rover.get_orientation(), 'N')
+        self.assertEqual(rover.get_width(), 10)
+        self.assertEqual(rover.get_height(), 10)
+
+    def test_illegal_setup(self):
+        negative_x = lambda: Rover(-1, 0)
+        negative_y = lambda: Rover(0, -1)
+        negative_width = lambda: Rover(width = -1)
+        negative_height = lambda: Rover(height = -1)
+        wrong_orientation = lambda: Rover(orientation = 'G')
+        self.assertRaises(ValueError, negative_x)
+        self.assertRaises(ValueError, negative_y)
+        self.assertRaises(ValueError, negative_width)
+        self.assertRaises(ValueError, negative_height)
+        self.assertRaises(ValueError, wrong_orientation)
+        
+
 if __name__ == '__main__':
     unittest.main()
